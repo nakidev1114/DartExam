2d91eeba0430d4ef46ef5ab697dedf9375231879
nakidev1114 <nakidev1114@gmail.com>
2024-10-15 21:47:29 +0900

    Test : Data Type (1)

---
+++ lib/exam.dart
@@ -0,0 +1,14 @@
+// 변수 타입 (1)
+void main() {
+
+  int age = 34;		// 정수형
+  double pi = 3.141592;	// 실수형
+  String name = 'Naki';	// 문자열
+  bool isTrue = true;	// 참 / 거짓
+
+  print('이름 : $name');
+  print('나이 : $age');
+  print('원주율 : $pi');
+  print('참 / 거짓 : $isTrue');
+
+}
