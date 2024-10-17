a0709c102c52011ed79851150b5b511b77de4c58
nakidev1114 <nakidev1114@gmail.com>
2024-10-17 21:17:03 +0900

    Test : Collection (1) - List

--- lib/exam.dart
+++ lib/exam.dart
@@ -13,15 +13,41 @@
 //
 // }
 
-// 변수 타입 (2)
+// // 변수 타입 (2)
+// void main() {
+//
+//   dynamic age = 34;
+//   print('나이 : $age');
+//   print(age.runtimeType);
+//
+//   age = '서른넷';
+//   print('나이 : $age');
+//   print(age.runtimeType);
+//
+// }
+
+// Collection (1) - List
 void main() {
 
-  dynamic age = 34;
-  print('나이 : $age');
-  print(age.runtimeType);
+  List<int> nakiBirth = []; // List<데이터 타입> 변수명 = [데이터1, 데이터2, ...];
+  print(nakiBirth);
 
-  age = '서른넷';
-  print('나이 : $age');
-  print(age.runtimeType);
+  nakiBirth.add(11);
+  nakiBirth.add(14);
+  print(nakiBirth);
+
+  // List<String> nakiLove = List.filled(5, 'who', growable : true);
+  // print(nakiLove);
+  //
+  // nakiLove.add('gf');
+  // print(nakiLove);
+  //
+  // nakiLove.remove('who');
+  // print(nakiLove);
+
+  List<String> nakiLove = ['who', 'who1', 'who2', 'who', 'gf'];
+
+  nakiLove.remove('who');
+  print(nakiLove);
 
 }
